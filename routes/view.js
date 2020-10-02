31e331aee96e06a4b2b53ffa22202a4cfc2c2fc7
catpea <corporate.miou@gmail.com>
2020-10-01 22:32:36 -0400

    New Release

--- routes/view.js
+++ routes/view.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/:id', async function(req, res, next) {
 
   if(req.params.id != req.params.id.replace(/[^a-z0-9_]/g,'')){
-    res.render('error', {message: 'Malformed id'});
+    res.render('error', {title:'Error', message: 'Malformed id'});
     return;
   }
 
@@ -14,20 +14,31 @@ router.get('/:id', async function(req, res, next) {
   } catch (err){
     if (err.name === 'not_found') {
       res.render('alert', {
+        title: 'Alert',
         message: 'The id does not yet exist, please create it by visiting the editor.',
-        html: [[`/edit/${req.params.id}`, `Edit ${req.params.id}`]].map(([url, label])=>`<a href="${url}">${label}</a>`).join('')
+        html: '',
+        buttons: [
+          {
+            url:`/edit/${req.params.id}`,
+            text:`Edit ${req.params.id}`
+          },
+          {
+            url:`/view/main`,
+            text:`Cancel`
+          }
+        ]
       });
     }else{
-      res.render('error', {message: 'Database error: 0eaf89fa-d600-4f28-8a41-7cb8016bcbce'});
+      res.render('error', {title:'Error', message: 'Database error: 0eaf89fa-d600-4f28-8a41-7cb8016bcbce'});
     }
     return;
   }
 
   if(doc){
-    const context = Object.assign({}, {title: 'Hello'}, doc);
+    const context = Object.assign({}, {title: req.params.id}, doc);
     res.render('index', context);
   }else{
-    res.render('error', {message: 'Database error: 8740cc6d-d53c-457e-9f9d-540956213889'});
+    res.render('error', {title:'Error', message: 'Database error: 8740cc6d-d53c-457e-9f9d-540956213889'});
   }
 
 
