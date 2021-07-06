3d587fb11022651733022e8b1b51a881ffbcb9ac
catpea <corporate.miou@gmail.com>
2021-07-05 21:37:43 -0400

    File cleanup.

--- _trash/edit.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('edit', { title: `Editing ${req.params.id}` });
-});
-
-export default router;
